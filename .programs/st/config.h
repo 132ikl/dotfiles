de5af2577c0575aedebd989eaba599ef9a1ffddd
132ikl <132@ikl.sh>
2019-08-23 05:50:09 +1000

    Add st config

--- .programs/st/config.h
+++ .programs/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "DejaVu Sans Mono:pixelsize=11:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -133,7 +133,7 @@ static const char *colorname[] = {
  * foreground, background, cursor, reverse cursor
  */
 unsigned int defaultfg = 7;
-unsigned int defaultbg = 257;
+unsigned int defaultbg = 0;
 static unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 
@@ -236,8 +236,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
-	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ ShiftMask,               XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ ShiftMask,               XK_Page_Down,   kscrolldown,    {.i = -1} },
 };
 
 /*
